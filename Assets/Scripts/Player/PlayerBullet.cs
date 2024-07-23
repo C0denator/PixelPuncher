85c113cadac30579173e0bbb028ddfe0fdaf625d
Mattis Thieme <mthieme2@hs-mittweida.de>
2024-07-24 00:03:57 +0200

    Health rework + Collider fix + Cleanup

--- Assets/Scripts/Player/PlayerBullet.cs
+++ Assets/Scripts/Player/PlayerBullet.cs
@@ -12,6 +12,9 @@ namespace GameProg.Player
         {
             //destroy the bullet max 5 seconds after it was created
             Destroy(gameObject, 5f);
+            
+            //set z position to 0
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
         }
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -30,6 +33,18 @@ namespace GameProg.Player
             if (!playerBullet && other.gameObject.CompareTag("Player"))
             {
                 Debug.Log("Player hit");
+                
+                //is player dashin?
+                if (other.gameObject.GetComponent<PlayerController>().IsDashing)
+                {
+                    //destroy the bullet
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    //deal damage to the player
+                    other.gameObject.GetComponent<PlayerController>().TakeDamage(1);
+                
                 Destroy(gameObject);
             }  
         }
