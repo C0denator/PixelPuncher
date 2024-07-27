fba20433df113c40c0f2e297cb83361a6bd876bd
Mattis Thieme <mthieme2@hs-mittweida.de>
2024-07-27 17:05:14 +0200

    Boss door only opens when player close

--- Assets/Scripts/World/BossDoor.cs
+++ Assets/Scripts/World/BossDoor.cs
@@ -1,25 +1,55 @@
+using System.Collections;
 using UnityEngine;
 namespace GameProg.World
 {
     public class BossDoor : MonoBehaviour
     {
+        [SerializeField] private Transform player;
         [SerializeField] private Animator animator;
         [SerializeField] private SpriteRenderer _spriteRenderer;
         [SerializeField] private BoxCollider2D _boxCollider2D;
+        [SerializeField] private float _openDistance;
         
+        private Door _door;
         private static readonly int IsOpen = Animator.StringToHash("IsOpen");
         private GameMaster _gameMaster;
+        
+        private Coroutine _openCoroutine;
         
         // Start is called before the first frame update
         void Awake()
         {
             _gameMaster = FindObjectOfType<GameMaster>();
+            player = GameObject.FindWithTag("Player").transform;
+            _door = GetComponent<Door>();
             
             //error handling
+            if (player == null) Debug.LogError("Player not set in BossDoor");
             if (_gameMaster == null) Debug.LogError("GameMaster not found in BossDoor");
             if (animator == null) Debug.LogError("Animator not set in BossDoor");
             if (_spriteRenderer == null) Debug.LogError("SpriteRenderer not set in BossDoor");
             if (_boxCollider2D == null) Debug.LogError("BoxCollider2D not set in BossDoor");
+            if (_door == null) Debug.LogError("Door not found in BossDoor");
+        }
+        
+        private void HandleOnAllRoomsCleared()
+        {
+            _openCoroutine = StartCoroutine(OpenCoroutine());
+            
+            //unsubscribe from the event
+            _gameMaster.OnAllRoomsCleared -= HandleOnAllRoomsCleared;
+        }
+        
+        private IEnumerator OpenCoroutine()
+        {
+            //is the player close enough?
+            while (Vector2.Distance(player.position, transform.position) > _openDistance || 
+                   (_door.roomA.World.CurrentRoom != _door.roomA && _door.roomB.World.CurrentRoom != _door.roomB))
+            {
+                yield return null;
+            }
+            
+            Open();
         }
 
         public void Open()
@@ -35,12 +65,15 @@ namespace GameProg.World
 
         void OnEnable()
         {
-            _gameMaster.OnAllRoomsCleared += Open;    
+            _gameMaster.OnAllRoomsCleared += HandleOnAllRoomsCleared;
         }
         
         void OnDisable()
         {
-            _gameMaster.OnAllRoomsCleared -= Open;
+            _gameMaster.OnAllRoomsCleared -= HandleOnAllRoomsCleared;
+            
+            //stop the coroutine
+            if(_openCoroutine != null) StopCoroutine(_openCoroutine);
         }
     }
 }
